311f25f340a3658fe939f0f1a02c5f467f0af861
Kun Wang <ifreedom.cn@gmail.com>
2017-06-26 18:21:06 +0800

    Add autostart.sh

---
+++ autostart.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+export LC_ALL=C
+
+if [ -z "$(type -t dirname)" ]; then
+    # Compute the dirname of FILE.
+    dirname () {
+        case ${1} in
+            */*) echo "${1%/*}${2}" ;;
+            *  ) echo "${3}" ;;
+        esac
+    }
+fi
+
+DIR="`dirname $0`"
+
+export PM2_HOME="$DIR/.pm2"
+
+$DIR/node-v8.1.2-linux-x64 $DIR/node_modules/.bin/pm2 startup
